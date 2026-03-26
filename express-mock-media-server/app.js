c9ec8418e92c61f18b742576b4b28efeea79f745
David Waring <david.waring2@bbc.co.uk>
2026-03-26 16:00:19 +0000

    Check for Not Modified and add test carousel document

--- express-mock-media-server/app.js
+++ express-mock-media-server/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var etag = require('etag');
 const bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
 
@@ -18,7 +19,32 @@ app.use(cookieParser());
 app.use(cors());
 //app.use(bodyParser.xml());
 app.use(bodyParser.text({ type: 'application/xml' }));
-app.use(express.static(path.join(__dirname, 'public'), {etag: true, index: false, lastModified: true, setHeaders(res, path, stat) { res.set('Content-Type', 'text/plain'); } }));
+app.use(express.static(path.join(__dirname, 'public'), {etag: true, index: false, lastModified: true, setHeaders(res, pth, stat) {
+		var not_modified = false;
+		var if_modified_since = res.req.get('If-Modified-Since');
+		if (if_modified_since !== undefined) {
+		    if (stat.mtime <= Date(if_modified_since)) {
+			not_modified = true;
+		    }
+		}
+		var if_none_match = res.req.get('If-None-Match');
+		if (if_none_match !== undefined) {
+		    var e = etag(stat);
+		    if (e == if_none_match) {
+			not_modified = true;
+		    }
+		}
+		if (not_modified) {
+		    res.status(304);
+		}
+		if (pth == path.join(__dirname, 'public/carousel')) {
+		    res.setHeader('Content-Type', 'application/3gpp-mbs-object-manifest+json;version="Rel17"');
+		} else {
+		    res.setHeader('Content-Type', 'text/plain');
+		}
+		res.setHeader('Cache-Control', 'max-age=30');
+	    }
+	}));
 
 app.use('/', indexRouter);
 
